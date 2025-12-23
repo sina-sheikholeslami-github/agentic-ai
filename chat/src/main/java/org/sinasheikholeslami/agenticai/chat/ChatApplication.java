ca52a71accbb12fc957e538a29b14e788fa7013c
Sheikholeslami <SiSheikholeslami@princesscruises.com>
2025-12-23 13:04:53 -0800

    add chat module

---
+++ chat/src/main/java/org/sinasheikholeslami/agenticai/chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package org.sinasheikholeslami.agenticai.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApplication.class, args);
+    }
+
+}
---
+++ chat/src/test/java/org/sinasheikholeslami/agenticai/chat/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sinasheikholeslami.agenticai.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
