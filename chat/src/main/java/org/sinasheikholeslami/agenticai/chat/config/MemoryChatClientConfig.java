c3ca45d69d67188ac79fcd518238aa54e5cdee61
Sheikholeslami <SiSheikholeslami@princesscruises.com>
2026-01-01 16:37:13 -0800

    add jdbc chat memory

--- chat/src/main/java/org/sinasheikholeslami/agenticai/chat/config/MemoryChatClientConfig.java
+++ chat/src/main/java/org/sinasheikholeslami/agenticai/chat/config/MemoryChatClientConfig.java
@@ -5,6 +5,8 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +14,14 @@ import java.util.List;
 
 @Configuration
 public class MemoryChatClientConfig {
+
+    @Bean
+    ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository) {
+        return MessageWindowChatMemory.builder()
+                .maxMessages(10)
+                .chatMemoryRepository(jdbcChatMemoryRepository)
+                .build();
+    }
 
     @Bean("memoryChatClient")
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
